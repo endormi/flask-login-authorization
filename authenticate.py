29e291e91c3fe857317aa3453797e3a462ea2e33
endormi <39559256+endormi@users.noreply.github.com>
2019-11-06 16:45:20 +0200

    Add sign in with POST method

--- authenticate.py
+++ authenticate.py
@@ -1,14 +1,33 @@
 from flask import Blueprint, redirect, url_for, render_template, flash, request
 from .users import __user__
-from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import login_user, logout_user, login_required
 from . import db
 
 
 authenticate = Blueprint('__auth__', __name__)
 
 
+@authenticate.route('/signin', methods=['POST'])
+def __signin__():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    checked = True if request.form.get('remember') else False
+
+    _user_ = __user__.query.filter_by(email=email).first()
+
+    if not _user_ or not check_password_hash(_user_.password, password):
+        flash('Email address or password is not correct.')
+        return redirect(url_for('__auth__.signin'))
+
+    login_user(_user_, remember=checked)
+
+    return redirect(url_for('base.user'))
+
+
 @authenticate.route('/signin')
 def signin():
+
     return render_template('sign_in.html')
 
 
@@ -34,9 +53,13 @@ def __signup__():
 
 @authenticate.route('/signup')
 def signup():
+
     return render_template('sign_up.html')
 
 
 @authenticate.route('/logout')
+@login_required
 def logout():
+    logout_user()
+
     return redirect(url_for('base.home'))
